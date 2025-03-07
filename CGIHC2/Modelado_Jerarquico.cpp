01ddee0376f08612ff3dee09dd46e191b83f9c34
refugiorangel <refugiorangel01@gmail.com>
2025-03-07 02:21:02 -0600

    CargaModelos

--- CGIHC2/Modelado_Jerarquico.cpp
+++ CGIHC2/Modelado_Jerarquico.cpp
@@ -48,7 +48,6 @@ int main() {
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-
 	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 
 	GLFWwindow *window = glfwCreateWindow(WIDTH, HEIGHT, "Rangel de la Rosa José Refugio", nullptr, nullptr);
